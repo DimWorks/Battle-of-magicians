ff42a4606fc768e2022f33807801343324e3c29a
DimWorks <dimulatoraz@gmail.com>
2023-08-05 00:23:26 +0700

    Добавил подтверждение выбора стихии

--- Battle of magicians/Battle of magicians.cpp
+++ Battle of magicians/Battle of magicians.cpp
@@ -8,25 +8,46 @@ using namespace std;
 int main()
 {
     RUS   
-    cout << "Добро пожаловать на битву магов!\n" 
-        << "Выберите стихию вашего героя:\n"
-        << "1. Огонь\n"
-        << "2. Вода\n"
-        << "3. Земля\n"
-        << "4. Воздух\n"
-        << "5. Выйти из игры\n"
-        << "Выш выбор: ";
+        bool isNotReady = true;
+    do {
+        cout << "Добро пожаловать на битву магов!\n"
+            << "Выберите стихию вашего героя:\n"
+            << "1. Огонь\n"
+            << "2. Вода\n"
+            << "3. Земля\n"
+            << "4. Воздух\n"
+            << "5. Выйти из игры\n"
+            << "Выш выбор: ";
 
-    int element = -1;
-    cin >> element;
-
-    if (element > 0 && element < 5)
-    {
-        tellStory(element);
-    }
-    else
-    {
-        exit(EXIT_SUCCESS);
-    }
+        int element = -1;
+        cin >> element;
+        if (element > 0 && element < 5)
+        {
+            tellStory(element);
+        }
+        else
+        {
+            exit(EXIT_SUCCESS);
+        }
+        cout << "Введите 1, чтобы подвердить выбор стихии\n"
+            << "Введите 2, чтобы вернуться к выбору стихии\n"
+            << "Введите любое число, чтобы выйти из игры\n";
+        int tmp = 0;
+        cin >> tmp;
+        switch (tmp)
+        {
+        case 1:
+            isNotReady = false;
+            break;
+        case 2:
+            isNotReady = true;
+            break;
+        default:
+            //cout << "ERROR\n";
+            exit(EXIT_SUCCESS);
+            break;
+        }
+    } while (isNotReady);
+    
 }
 
--- Battle of magicians/MyLib.h
+++ Battle of magicians/MyLib.h
@@ -1,3 +1,11 @@
 #pragma once
 
+typedef struct hero
+{
+	int munck;
+	char element;
+	int health;
+	int power;
+}hero;
+
 void tellStory(int element);
